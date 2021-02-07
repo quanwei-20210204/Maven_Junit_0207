0b43fc1d53041abedf5440b75ba0d7c172c752f0
quanwei-20210204 <25513275@qq.com>
2021-02-07 12:51:25 +0800

    add pom and src directory including HelloSAP.java

---
+++ src/main/java/org/vic/HelloSAP.java
@@ -0,0 +1,14 @@
+package org.vic;
+
+import java.util.*;
+
+public class HelloSAP {
+    public static void main(String[] args){
+        Hello_world();
+    }
+
+    public static void Hello_world(){
+        System.out.println("Hello World!");
+        System.out.println(new Date());
+    }
+}
