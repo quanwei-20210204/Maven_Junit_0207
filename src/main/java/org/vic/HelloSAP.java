f70b701cf90e1e72b3c915ccd04ef8c9374f7197
quanwei-20210204 <25513275@qq.com>
2021-02-08 11:42:14 +0800

    task 2-2, print number from 1 to 100, when meet 3 and 3*n, print "fizz", 5 and 5*n, print "buzz"

--- src/main/java/org/vic/HelloSAP.java
+++ src/main/java/org/vic/HelloSAP.java
@@ -8,9 +8,17 @@ public class HelloSAP {
     }
 
     public static void Hello_world(){
-        //task 2-1, print number from 1 to 100
-        for (int i = 0; i < 100; i++){
-            System.out.println(i+1);
+        //task 2-2, print number from 1 to 100, when meet 3 and 3*n, print "fizz", 5 and 5*n, print "buzz"
+        for (int i = 1; i <= 100; i++){
+
+            if(i % 3 == 0) {
+                System.out.println("fizz");
+            } else if(i % 5 == 0) {
+                System.out.println("buzz");
+            } else{
+                System.out.println(i);
+            }
+
 
         }
         //System.out.println("Hello World!");
