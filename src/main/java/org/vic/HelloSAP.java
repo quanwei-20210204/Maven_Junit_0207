f3b864c598979e9e2e9294e1c892b9671f2f4e66
quanwei-20210204 <25513275@qq.com>
2021-02-09 12:14:58 +0800

    New a array to contain the result, not print it on the terminal directly. and for Junit test, it's easier to test automatically, usually using assert, not manually.

--- src/main/java/org/vic/HelloSAP.java
+++ src/main/java/org/vic/HelloSAP.java
@@ -3,24 +3,36 @@ package org.vic;
 import java.util.*;
 
 public class HelloSAP {
+    static Collection<String> collection = new ArrayList<String>();
+
     public static void main(String[] args){
         Hello_world();
     }
 
     public static void Hello_world(){
         //task 2-2, print number from 1 to 100, when meet 3 and 3*n, print "fizz", 5 and 5*n, print "buzz"
+
+
+
         for (int i = 1; i <= 100; i++){
 
             if(i % 3 == 0) {
-                System.out.println("fizz");
+                collection.add("fizz");
+                //System.out.println("fizz");
             } else if(i % 5 == 0) {
-                System.out.println("buzz");
+                collection.add("buzz");
+                //System.out.println("buzz");
             } else{
-                System.out.println(i);
+                collection.add(Integer.toString(i));
+                //System.out.println(i);
             }
 
-
         }
+
+        for(String i: collection){
+            System.out.println(i);
+        }
+
         //System.out.println("Hello World!");
         //System.out.println(new Date());
     }
--- src/test/java/org/vic/HelloSAPTest.java
+++ src/test/java/org/vic/HelloSAPTest.java
@@ -1,12 +1,17 @@
 package org.vic;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 //task 2-3, unit test for class HelloSAP, only one method
 public class HelloSAPTest extends TestCase {
+    static Collection<String> collectionTest = new ArrayList<String>();
 
     private static HelloSAP h=new HelloSAP();
 
@@ -23,6 +28,8 @@ public class HelloSAPTest extends TestCase {
 
     @Test
     public void testHello_world() {
+        collectionTest.add("buzz");
         h.Hello_world();
+        Assert.assertEquals(collectionTest,HelloSAP.collection);
     }
 }
