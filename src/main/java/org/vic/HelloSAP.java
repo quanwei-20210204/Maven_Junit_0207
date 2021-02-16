718ed8e3fe880e23a39141e91ddcb30b691ebd91
quanwei-20210204 <25513275@qq.com>
2021-02-16 11:54:14 +0800

    task 2-6 receive the second parameter for the number of players, my position is random

--- src/main/java/org/vic/HelloSAP.java
+++ src/main/java/org/vic/HelloSAP.java
@@ -8,25 +8,30 @@ public class HelloSAP {
     public static void main(String[] args){
 
         HelloSAP hsap = new HelloSAP();
-        hsap.hello_world(10);
+        hsap.hello_world(10,7);
 
     }
 
     public  Collection<String> hello_world(){
-        return hello_world(100);
+        return hello_world(100,9);
     }
 
-    public Collection<String> hello_world(int number){ //return value will be the result;
+    public Collection<String> hello_world(int number,int players){ //return value will be the result;
         //task 2-2, print number from 1 to 100, when meet 3 and 3*n, print "fizz", 5 and 5*n, print "buzz"
         //task 2-4, one parameter for hello_world(), means the top value for the times we print;  check the parameter
         if(number < 0) {
             throw new ArithmeticException("the number should be positive.");
         }
 
+        //task 2-6 receive the second parameter for the number of players, my position is random
+        Random random = new Random();
+        int myposition = random.nextInt(players)+1;
+        System.out.println(myposition);
+
         for (int i = 1; i <= number; i++){
 
             //task 2-5: command line prompt:
-            System.out.println("please input your answer:");
+           /* System.out.println("please input your answer:");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.next();
 
@@ -36,7 +41,7 @@ public class HelloSAP {
             }else{
                 System.out.println("wrong answer");
                 return null;
-            }
+            }*/
 
             if(i % 3 == 0) {
                 collection.add("fizz");
@@ -51,9 +56,9 @@ public class HelloSAP {
 
         }
 
-        for(String i: collection){
+        /*for(String i: collection){
             System.out.println(i);
-        }
+        }*/
 
         return collection;
         //System.out.println("Hello World!");
