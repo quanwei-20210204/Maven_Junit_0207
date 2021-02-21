cb4ce4c71f7665ca7276bbfae885c8ed8b067e43
quanwei-20210204 <25513275@qq.com>
2021-02-21 11:57:37 +0800

    task 2-2: Rewrite it, reference to Vic's code; Implement a simple one sub-task: number divided by 3, show "fizz" Extract duplicated codes to a method, like vary();

--- src/main/java/org/vic/HelloSAP.java
+++ src/main/java/org/vic/HelloSAP.java
@@ -4,32 +4,47 @@ import java.util.*;
 
 //task 2-2: rewrite the task, divide the task into two parts, one is the logic, main method for present;
 public class HelloSAP {
-    private Collection<String> collection = new ArrayList<String>();
-
+    public static final String FIZZ = "fizz";
 
     //Comments from Vic: main method for View -- the input and output, and present
     public static void main(String[] args){
+
+        HelloSAP hSAP = new HelloSAP();
+        hSAP.countOff();
 
     }
 
     // Method name: countOff
     public void countOff(){
-        for (int i = 1; i <= 6; i++){
-            if(i % 3 == 0){
-                collection.add("fizz");
-            }else {
-                collection.add(Integer.toString(i));
-            }
-        }
-    }
+        List<String>  outcome = new ArrayList<>(100);
 
-    //show the result in List
-    public void showResult(){
-        for(String s:collection){
+        for (int i = 1; i <= 100; i++){
+
+            outcome.add(vary(i));
+
+        }
+
+        for(String s: outcome){
             System.out.println(s);
         }
 
+    }
 
+    String vary(int i){
+        if(i % 3 == 0){
+            return FIZZ;
+        }else{
+            return Integer.toString(i);
+        }
+    }
+
+
+/*
+    //show the result in List
+    public void showResult(){
+        for(String s:){
+            System.out.println(s);
+        }
     }
 
     //show the list's size
@@ -43,6 +58,6 @@ public class HelloSAP {
     }
 
     //Model or the logic is separated from View, in order to divide into two parts.
-
+*/
 }
 
