4b3ea06daafa4d201c9f940a1112b50ecfe7249e
quanwei-20210204 <25513275@qq.com>
2021-02-23 12:32:22 +0800

    1.Junit4 rules; 2.parameterized test

--- src/test/java/org/vic/HelloSAPTest.java
+++ src/test/java/org/vic/HelloSAPTest.java
@@ -1,33 +1,51 @@
 package org.vic;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
 import junit.framework.Assert;
-import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+//import junit.framework.TestCase;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 //task 2-3, unit test for class HelloSAP, only one method
-public class HelloSAPTest extends TestCase {
+@RunWith(Parameterized.class)
+public class HelloSAPTest /*extends TestCase*/ {
 
     public HelloSAP helloSAP = new HelloSAP();
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
+    @Parameterized.Parameters
+    public static Collection testData(){
+        return Arrays.asList(new Object[][]{{"1",1},{"2",2},{"fizz",3}});
+    }
+
+
+    private String hExpected;
+    private int hInput;
+    public HelloSAPTest(String expected,int input){
+        hExpected = expected;
+        hInput = input;
+    }
+
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        //super.setUp();
+
         System.out.println("test begin");
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterClass
+    public static void tearDown() throws Exception {
         System.out.println("test ends");
     }
 
-    @Test
-    public void testCountOff() {
+    @Ignore
+    public void countOff() {
 
         List<String> testOutcome = new ArrayList<>();
 
@@ -40,9 +58,16 @@ public class HelloSAPTest extends TestCase {
     }
 
     @Test
-    public void testVary() {
+    public void vary() {
 
         Assert.assertEquals("fizz",helloSAP.vary(3));
+        Assert.assertEquals("buzz",helloSAP.vary(5));
+        Assert.assertEquals("fizzbuzz",helloSAP.vary(15));
+        Assert.assertEquals("7",helloSAP.vary(7));
+
+        Assert.assertEquals(hExpected,helloSAP.vary(hInput));
+
+
 
     }
 
